3d01f31cc298e48547b66345c1fb3814a41b0e22
GeorginaMoraa <georginagee02@gmial.com>
2025-03-20 11:05:43 +0300

    migrations

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class MemberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'role' => fake()->randomElement(['father', 'mother', 'son', 'daughter', 'grandfather', 'grandmother', 'grandson', 'granddaughter', 'uncle', 'aunt', 'nephew', 'niece', 'cousin', 'friend']),
+            'user_id' => 1,
+            'family_id' => 1,
+        ];
+    }
+}
